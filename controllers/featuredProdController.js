8518f8d21c67315be0d375817effbada3f0c3402
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2024-10-30 22:56:15 +0530

    updated APIs

--- controllers/featuredProdController.js
+++ controllers/featuredProdController.js
@@ -2,18 +2,27 @@ import FeaturedProdModel from '../models/featuredProdModel';
 
 // get all featured products
 
-// save ids of featured products entered in any of the 
+// an array of ids are received and saved which can be retreived as a whole array
 
+export const saveFeaturedProd = async (req, res) => {
+    const featuredId = [...req.body];
+
+    const products = await FeaturedProdModel.save({ productid: featuredId });
+
+    if (!products) res.status(404).json({ message: 'Could Not Save Featured Products!' });
+
+    res.status(200).json({ products });
+};
 
 export const getFeaturedProd = async (req, res) => {
     try {
-        const products = FeaturedProdModel.find();
+        const products = await FeaturedProdModel.find().select('productId');
 
-        if(!products) res.status(404).json({ message: 'No Featured Products Available'});
+        if (products.length === 0) res.status(404).json({ message: 'No Featured Products Available' });
 
         res.status(200).json({ products });
     } catch (err) {
         console.error(error);
-        res.status(500).json({ message: 'Internal Server Error!'});
+        res.status(500).json({ message: 'Internal Server Error!' });
     }
-}
+};
--- models/featuredProdModel.js
+++ models/featuredProdModel.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
 const featuredProdSchema = mongoose.Schema({
-    featuredId: { type: String, required: true },
-    productId: { type: String, required: true }
+    productId: { type: [String], required: true }
 });
 
 export const FeaturedProd = mongoose.model('FeaturedProd', featuredProdSchema);
