9071823f07187c13cd0496ea1c50da914168479c
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2024-11-01 23:13:46 +0530

    updated API

--- controllers/inquiryController.js
+++ controllers/inquiryController.js
@@ -1,31 +1,10 @@
 import Inquiry from '../models/inquiryModel';
-
+import { getNextSequenceValue } from './idCounterController';
+// save inquiry
 // get all Inquiries
 // search filter including all types in one endpoint
 // send the state of all filters which are initially 'all' / values when changed
 // get all full names of customers/inquiryId to list in a datalist
-
-export const getInquiry = async (req, res) => {
-    const { subject, date, responded } = req.body;
-    let filter = {};
-
-    subject !== 'all' && (filter.subject = subject);
-    if (date !== 'all') {
-        filteredDate = dateFilter(date);
-        filter.date = filteredDate.date;
-    };
-    responded !== 'all' && (filter.responded = responded);
-
-    try {
-        const inquirys = await Inquiry.find(filter).sort(filteredDate.sortedDate ? filteredDate.sortedDate : {});
-
-        if (!inquirys) res.status(404).json({ message: 'Could Not Find Any Inquiries!' });
-        res.status(200).json({ inquirys });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error!', error });
-    }
-};
 
 const dateFilter = (date) => {
     const filteredDate = {};
@@ -58,12 +37,55 @@ const dateFilter = (date) => {
     }
 }
 
+export const saveInquiry = async (req, res) => {
+    const { fullName, companyName, email, mobileNumber, message } = req.body;
+
+    try {
+        let inquiryId = getNextSequenceValue('inquiry');
+        const id = 'INQ-' + inquiryId;
+        const inquiry = await Inquiry.save({ inquiryId: id, fullName: fullName, companyName: companyName, email: email, mobileNumber: mobileNumber, message: message })
+
+        if (!inquiry) res.status(404).json({ message: 'Could Not Save Inquiry, Please Try Again!' });
+        res.status(200).json(inquiry);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error!', error });
+    }
+};
+
+export const getInquiry = async (req, res) => {
+    const { subject, date, responded } = req.body;
+    let filter = {};
+
+    subject !== 'all' && (filter.subject = subject);
+    if (date !== 'all') {
+        filteredDate = dateFilter(date);
+        filter.date = filteredDate.date;
+    };
+    responded !== 'all' && (filter.responded = responded);
+
+    try {
+        const inquirys = await Inquiry.find(filter).sort(filteredDate.sortedDate ? filteredDate.sortedDate : {});
+
+        if (!inquirys) res.status(404).json({ message: 'Could Not Find Any Inquiries!' });
+        res.status(200).json({ inquirys });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error!', error });
+    }
+};
+
+
 export const getInquiryByIdAndName = async (req, res) => {
     const { id, name } = req.body;
 
+    try {
+        const inquiry = await Inquiry.find(id !== '' && { inquiryId: id } + name !== '' && { fullName: name });
 
+        if (!inquiry) res.status(404).json({ message: 'Could Not Find An Inquiry with the ID: ' + id });
+        res.status(200).json({ inquiry });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error!', error });
+    }
 }
-
-// export const getInquiry = async () => {
-
-// }
